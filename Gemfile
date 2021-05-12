9d4f5bb3cc226049034a4d0e65831349f7ca4f32
Tomoki Kobayashi <tomoki58840725@gmail.com>
2021-05-12 07:12:11 +0000

    05 create rogin system

--- Gemfile
+++ Gemfile
@@ -35,6 +35,15 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+#deviseを使用
+gem 'devise'
+
+#bootstrapを使用
+gem 'bootstrap', '~> 4.6.0'
+gem 'jquery-rails'
+
+gem 'dotenv-rails', '~> 2.7', '>= 2.7.6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-resources :products
+  
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks'
+  }
+  
+  devise_scope :user do
+    root "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+  
+  resources :products
 
-root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
