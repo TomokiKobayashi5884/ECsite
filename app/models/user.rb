c3017910a7c4610c0a3b47bca9c92f65905ef2dd
Tomoki Kobayashi <tomoki58840725@gmail.com>
2021-05-20 03:49:56 +0000

    15 add_display_favorite_products

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   acts_as_liker
   
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }}
   
 def update_password(params, *options)
   if params[:password].blank?
